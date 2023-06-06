24258e5957fee474af40d7d8ec0d5d31f097197e
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2023-06-06 05:42:44 +0100

    my first server

---
+++ js/nodejs/server.js
@@ -0,0 +1,28 @@
+const express=require("express");
+const app=express();
+
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req,res)=>{
+	res.send(`
+	<form action="/answer" method="POST">
+		<label for="skyColor">What is the color of the sky on a bright sunny day?</label>
+		<input name="skyColor" autocomplete="off"/>
+		<input type="submit" name="submit answer"/>
+	</form>
+		`);
+});
+app.post('/answer', (req, res)=>{
+	req.body.skyColor.toUpperCase()=="BLUE"? res.send(`
+	<p>Congrats, that$apos;s the correct answer</p>
+	<a href="/">Back to home page</a>
+	`) : res.send(`
+	<p>Sorry, that&apos;s incorrect</p>
+	<a href="/">Try again</a>
+		`)
+});
+app.get('/answer', (req, res)=>{
+	res.send('Are you lost?, there is nothing here');
+});
+app.listen(2023);
+console.log('phast inc');
