82c7d63beda3f2b1e7f135d11eada2a03d174023
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2023-06-15 21:23:42 +0100

    event emitter

--- js/nodejs/eventLoop/03.js
+++ js/nodejs/eventLoop/03.js
@@ -2,7 +2,10 @@ const EmitEvent=require('events');
 
 const customEmitter=new EmitEvent();
 
-customEmitter.on('response', ()=>{
-	console.log(`data received`)
+customEmitter.on('response', (name, id)=>{
+	console.log(`data received: ${name} with id: ${id}`)
 });
-customEmitter.emit('response')
+customEmitter.on('response', ()=>{
+	console.log(`some other function`)
+});
+customEmitter.emit('response', 'Anuoluwapo', 35)
